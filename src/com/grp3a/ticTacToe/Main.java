24f8c8e6576b9e750b8a12808f2eaa6a4939e8db
JJspa <JJspa@DESKTOP-053R101>
2021-10-27 00:15:52 +0200

    Commit Ny Frame class Implemented

--- src/com/grp3a/ticTacToe/Main.java
+++ src/com/grp3a/ticTacToe/Main.java
@@ -1,14 +1,5 @@
 package com.grp3a.ticTacToe;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -16,28 +7,31 @@ public class Main {
 ///////////////////////////////////Idé att vi ska gör GUI Lösning////////////////////////////////
 		
 		
-		JFrame frame = new JFrame("Välkommen");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JPanel primary = new JPanel();
-		primary.setBackground(Color.red);
-		primary.setPreferredSize(new Dimension(500,500));
+		//JFrame frame = new JFrame("Välkommen");
+//		
+//		JPanel primary = new JPanel();
+//		primary.setBackground(Color.red);
+//		primary.setPreferredSize(new Dimension(500,500));
+//		
+//		frame.getContentPane().add(primary);
+//		frame.pack();// viktig
+//		frame.setVisible(true);// viktig	
+///////////////////////////////////////Ännu Nyare///////////////////////////////////////////////
 		
-		frame.getContentPane().add(primary);
-		frame.pack();// viktig
-		frame.setVisible(true);// viktig	
+		new ThaFrame();//skapar Jframe Från en egen class.
 		
 ///////////////////////////////////////Knapp Test  1   /////////////////////////////////////////	
 		
-		JButton button1 = new JButton("Ruta 0,0 ");
-		
-		button1.setBounds(10, 10, 100, 100);
-		frame.add(button1);
-		frame.setLayout(null);
-		button1.addActionListener(new ActionListener(){  
-			public void actionPerformed(ActionEvent e){  
-			           System.out.println("Ruta 0,0 == Clicked");  
-			        }  
-			    });  
+//		JButton button1 = new JButton("Ruta 0,0 ");
+//		
+//		button1.setBounds(10, 10, 100, 100);
+////		frame.add(button1);
+////		frame.setLayout(null);
+//		button1.addActionListener(new ActionListener(){  
+//			public void actionPerformed(ActionEvent e){  
+//			           System.out.println("Ruta 0,0 == Clicked");  
+//			        }  
+//			    });  
 
 	
 	}
---
+++ src/com/grp3a/ticTacToe/ThaFrame.java
@@ -0,0 +1,39 @@
+package com.grp3a.ticTacToe;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+//I stort sätt samma som innan men Construktas från sin egen Class
+public class ThaFrame extends JFrame implements ActionListener{
+	
+	JButton button;// knapp
+	
+	ThaFrame() {
+		
+		button = new JButton();
+		button.setBounds(100, 100, 100, 100);
+		button.addActionListener(this);
+		button.setText("Btn 1");
+		button.setFocusable(false);
+		
+		this.setTitle("Tic-Tac-Toe :  Grp3a.");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(null);
+		this.setSize(500, 500);// förnster storlek
+		this.setVisible(true);
+		this.add(button);
+	}
+	// vad som händer när man trycker på knappen i nuläget inget mer en skriver ut 
+	// Test i konsolen
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==button) {
+			System.out.println("test");
+		}
+		
+	}
+}
