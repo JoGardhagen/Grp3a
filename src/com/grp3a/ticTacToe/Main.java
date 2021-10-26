5da62710ae15788482133f21d4a83b4f855e4362
JJspa <JJspa@DESKTOP-053R101>
2021-10-26 15:03:54 +0200

    Commit Ny Form

--- src/com/grp3a/ticTacToe/Main.java
+++ src/com/grp3a/ticTacToe/Main.java
@@ -1,11 +1,17 @@
 package com.grp3a.ticTacToe;
 
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// main classen
 		// nytt från nu : ) 
-
+		JFrame frame = new JFrame();
+		frame.setSize(300,400);
+		frame.setTitle("Tic-Tac-Toe : Grp3a");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 }
