c0de449046b0d33a51b9314b195559be37c9db2b
JJspa <JJspa@DESKTOP-053R101>
2021-11-01 15:55:47 +0100

    Commit reset BTn Ish

--- src/com/grp3a/ticTacToe/Main.java
+++ src/com/grp3a/ticTacToe/Main.java
@@ -2,10 +2,13 @@ package com.grp3a.ticTacToe;
 
 public class Main {
 
+
 	public static void main(String[] args) {
 
 		
 		new ThaFrame2(); // skapar ett Objekt av ThaFrame klassen som JFrame
+		
+		
 		//new ThaFrame();
 		//new EvenAnotherFrame();// Ny JFrame För att öppna Både den första Thaframe Och Andra 
 							   // ThaFrame2.
--- src/com/grp3a/ticTacToe/ThaFrame2.java
+++ src/com/grp3a/ticTacToe/ThaFrame2.java
@@ -26,6 +26,7 @@ public class ThaFrame2 extends JFrame implements ActionListener {
 	JPanel topPanel = new JPanel();
 	boolean cross;//bools variable för 'X'
 	Random rand = new Random();
+	JButton resetButton = new JButton(); // resetKnapp för att resetta skiten
 	
 	
 	ThaFrame2() {
@@ -39,12 +40,18 @@ public class ThaFrame2 extends JFrame implements ActionListener {
 			button[i].addActionListener(this);
 			button[i].setText("");
 		}
-
+		topPanel.setLayout(new BorderLayout());// borderLayout för topPanelen
 		lable.setBackground(Color.ORANGE);
 		lable.setText("Test Lable");
-		topPanel.add(lable);
+		topPanel.add(lable,BorderLayout.WEST);// sätter test Lable till vänster 
+		
 		topPanel.setPreferredSize(new Dimension(10, 50));
 		topPanel.setBackground(Color.ORANGE);
+		
+		resetButton.setPreferredSize(new Dimension(100,40));// dimension för knappen
+		resetButton.setText("Reset");// texten som står på knappen
+		resetButton.addActionListener(this);
+		topPanel.add(resetButton,BorderLayout.EAST); // sätter ResetKnappen till höger .
 
 		frame.setTitle("Tic-Tac-Toe :  Grp3a.");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,17 +62,8 @@ public class ThaFrame2 extends JFrame implements ActionListener {
 		frame.add(buttonsPanel, BorderLayout.CENTER);// paneler gör det möjligt att lägga saker var man vill :)
 		frame.add(topPanel, BorderLayout.NORTH);
 
-	}//gör inget just nu 
-//	public void AiRoll() {
-//		for (int i = 0; i < 9; i++) {
-//			int aiRoll = rand.nextInt(8);// generar ett slumptal 
-//			if (button[aiRoll].getText() == "") {// kastar in talet som index i arrayenn ser om elementet för index är blankt 
-//				button[aiRoll].setText("O");// om det är blankt set text Till 'O'
-//				cross = false;	//cross false 
-//				}
-//		}
-//	}
 	
+	}
 	// vad som händer när man trycker på knappen i nuläget inget mer en skriver ut
 	// Test i konsolen
 	// ändrar nu Tecken till Kryss
@@ -75,11 +73,11 @@ public class ThaFrame2 extends JFrame implements ActionListener {
 			if (e.getSource() == button[i]) {
 				if (cross) {//när true är det 'O' tur, 
 					//AiRoll();
-					int aiRoll = rand.nextInt(8);// generar ett slumptal 
-					if (button[aiRoll].getText() == "") {// kastar in talet som index i arrayenn ser om elementet för index är blankt 
-						button[aiRoll].setText("O");// om det är blankt set text Till 'O'
-						cross = false;	//cross false 
-						}
+//					int aiRoll = rand.nextInt(8);// generar ett slumptal 
+//					if (button[aiRoll].getText() == "") {// kastar in talet som index i arrayenn ser om elementet för index är blankt 
+//						button[aiRoll].setText("O");// om det är blankt set text Till 'O'
+//						cross = false;	//cross false 
+//						}
 					// funkar något men man måste spamma på en knapp för att han skall generera 
 					// ett en AI Roll.....
 					// tittar på lösning i skrivande stund :)   
@@ -89,13 +87,26 @@ public class ThaFrame2 extends JFrame implements ActionListener {
 					if (button[i].getText() == "") {
 						button[i].setText("X");
 						cross = true;//vi kan säga logiken är när Kryss lagt sitt svar är Boolska variabeln == True
-						
+						int aiRoll = rand.nextInt(8);// generar ett slumptal 
+						if (button[aiRoll].getText() == "") {// kastar in talet som index i arrayenn ser om elementet för index är blankt 
+							button[aiRoll].setText("O");// om det är blankt set text Till 'O'
+							cross = false;	//cross false 
+							}
 					}
 				}
 
 			}
 			
 
+		}// reseten fungerar men ändå inte öppnar bara ett nyttfönster som det är nu men öm ja 
+		// fått ihop något iallfall :) 
+		if(e.getSource()==resetButton) {
+			this.dispose();
+			
+			new ThaFrame2();
+//			frame.getContentPane().invalidate();
+//			frame.getContentPane().validate();
+//			frame.getContentPane().repaint();
 		}
 	}
 }
