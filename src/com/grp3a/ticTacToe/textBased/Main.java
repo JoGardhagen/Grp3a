6264b7ab98a9f403e22c2765bf00fa114697b494
nerai <nerai@LAPTOP-5Q7PRKVR.lan>
2021-11-02 14:55:57 +0100

    uppe

--- src/com/grp3a/ticTacToe/textBased/Main.java
+++ src/com/grp3a/ticTacToe/textBased/Main.java
@@ -1,10 +1,39 @@
 package com.grp3a.ticTacToe.textBased;
 
+import java.util.*;
+
 public class Main {
+//    ||
+//    ||
+//     | |
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    static char [][] playBoard = new char [3][3];
 
-	}
+    public static void main(String[] args) {
+    	Random rand= new Random();
+    	Scanner scan = new Scanner(System.in);
+    
+    
+    int row =scan.nextInt();
+    int column =scan.nextInt();
 
+    playBoard[row][column]= 'X';
+    
+    printGameBoard();
+    int airoll=rand.nextInt(2);
+    playBoard[airoll][airoll]='O';
+    printGameBoard();
+    }
+// Måla ut spelplanen med hjälp av en nestlad for-loop 3x3
+    public static void printGameBoard() {
+    System.out.println("");
+    for(int rad =0; rad<3;rad++) {
+        for(int pel=0;pel<3;pel++) {
+
+            System.out.print("["+playBoard[rad][pel] + "]"+rad+","+pel);
+        }
+    System.out.println();
+        }
+    }
 }
+
