59f797273a40b1430ad2aab48d06b626a442de7e
JJspa <JJspa@DESKTOP-053R101>
2021-11-02 20:06:43 +0100

    Commit Ai Roll Method

--- src/com/grp3a/ticTacToe/textBased/Main.java
+++ src/com/grp3a/ticTacToe/textBased/Main.java
@@ -3,15 +3,17 @@ package com.grp3a.ticTacToe.textBased;
 import java.util.*;
 
 public class Main {
-/*Check Lista för arbetet :: // notera klar när klar. 
- * ? switch stat för aiRoll. : Jocke Fixar
- * ? gameLoop för att få spelet att fortsätta : Anna Fixar
- * ? win or Lose logit.
- * ? replay funk  
- * 
- * 
- * 
- * */
+	/*
+	 * Check Lista för arbetet :: // notera klar när klar. 
+	 * ? switch stat för aiRoll. : Jocke Fixar  // Note Fixat
+	 * ? gameLoop för att få spelet att fortsätta : Anna Fixar 
+	 * ? win
+	 * or Lose logit. 
+	 * ? replay funk
+	 * 
+	 * 
+	 * 
+	 */
 //    ||
 //     | |
 
@@ -24,26 +26,27 @@ public class Main {
 		System.out.println("skriv koordinater För 'X' 1 till 9");
 
 		int xCordinate = scan.nextInt();// tar emot ett värde istället för 2.
-		XMove(xCordinate);// anropar XMove metod som omvandlar xCor till 2 värden och 
-					//slänger in 'X' i Playboard
+		XMove(xCordinate);// anropar XMove metod som omvandlar xCor till 2 värden och
+		// slänger in 'X' i Playboard
 
 		printGameBoard();
 		//
 		System.out.println("Computer 'O'");
-		int airoll = rand.nextInt(2);
-		playBoard[airoll][airoll] = 'O';
+		int aIRoll = rand.nextInt(9);
+		OMove(aIRoll);
+		//playBoard[airoll][airoll] = 'O';
 		printGameBoard();
 		scan.close();
 	}
 
 // Måla ut spelplanen med hjälp av en nestlad for-loop 3x3
 	public static void printGameBoard() {
-		//System.out.println("");
+		// System.out.println("");
 		for (int rad = 0; rad < 3; rad++) {
 			for (int pel = 0; pel < 3; pel++) {
 
-				//System.out.print("[" + playBoard[rad][pel] + "]" + rad + "," + pel);
-				System.out.print("[" + playBoard[rad][pel] + "]" );// som innan fast visar inte x o y Korrdinaterna
+				// System.out.print("[" + playBoard[rad][pel] + "]" + rad + "," + pel);
+				System.out.print("[" + playBoard[rad][pel] + "]");// som innan fast visar inte x o y Korrdinaterna
 			}
 			System.out.println();
 		}
@@ -100,6 +103,62 @@ public class Main {
 			row = 2;
 			column = 2;
 			return playBoard[row][column] = 'X';
+
+		}
+		return 0;
+
+	}
+
+	public static char OMove(int aIRoll) {
+		int row;
+		int column;
+
+		// switch sats som omvandlar det användaren skriver in till x o y koordinater
+		switch (aIRoll) {
+		case 1:
+			row = 0;
+			column = 0;
+			return playBoard[row][column] = 'O';
+
+		case 2:
+			row = 0;
+			column = 1;
+			return playBoard[row][column] = 'O';
+
+		case 3:
+			row = 0;
+			column = 2;
+			return playBoard[row][column] = 'O';
+
+		case 4:
+			row = 1;
+			column = 0;
+			return playBoard[row][column] = 'O';
+
+		case 5:
+			row = 1;
+			column = 1;
+			return playBoard[row][column] = 'O';
+
+		case 6:
+			row = 1;
+			column = 2;
+			return playBoard[row][column] = 'O';
+
+		case 7:
+			row = 2;
+			column = 0;
+			return playBoard[row][column] = 'O';
+
+		case 8:
+			row = 2;
+			column = 1;
+			return playBoard[row][column] = 'O';
+
+		case 9:
+			row = 2;
+			column = 2;
+			return playBoard[row][column] = 'O';
 
 		}
 		return 0;
